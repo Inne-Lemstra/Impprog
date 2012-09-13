30d840980b513acdbb301e74e5c26a17604f2a72
HanKruiger <hankruiger@gmail.com>
2012-09-13 15:49:47 +0200

    Begin

--- week2/faculteit.c
+++ week2/faculteit.c
@@ -1,1 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int invoer;
+  long resultaat = 1;
+  printf("Van welk getal moet de faculteit worden berekend: ");
+  scanf("%d", &invoer);
+  int k;
+  for (k = 1; k < invoer; k++) {
+    resultaat *= k;
+  }
+  
+  printf("De faculteit van %d is %l", invoer, resultaat);
+  
+  return 0;
+}
