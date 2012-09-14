bff268359811926b3e03ad04784f5c1dad440da2
Inne-Lemstra <innelemstra@msn.com>
2012-09-14 09:06:26 +0200

    variabelen van naam verandert voor minder typen

--- week2/markt/markt.c
+++ week2/markt/markt.c
@@ -11,17 +11,30 @@ int main() {
   printf("De prijs van een meloen     : ");
   scanf("%d", &meloenPrijs);
   
-  maxSinaasappels = 100000 / sinaasappelPrijs;
-  maxGrapefruits = 100000 / grapefruitPrijs;
-  maxMeloenen = 100000 / meloenPrijs;
+  int maxsinaas, maxgrape ,maxmel
+  
+  maxsinaas = 10000 / sinaasappelPrijs;
+  maxgrape = 10000 / grapefruitPrijs;
+  maxmel = 10000 / meloenPrijs;
   
   printf("Sinaasappel\tGrapefruit\tMeloen");
   
-  int sinaasappelIndex;
-  int grapefruitIndex;
-  int meloenIndex;
+  int sinaasindex;
+  int grapeindex;
+  int melindex;
   
-  for (sinaasappelIndex = 0; sinaasappelIndex <= maxSinaasappels; sinaasappelIndex++) {
+  for(sinaasIndex=0; sinaasindex<=maxsinaas; sinaasindex++){
+	maxgrape=10000
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  for (sinaasappelindex = 0; sinaasappelindex <= maxsinaasappels; sinaasappelindex++) {
     
   }
   
