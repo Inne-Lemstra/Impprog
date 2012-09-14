b1bae5433a0c4af350230327e2e5bef0ef50045e
HanKruiger <hankruiger@gmail.com>
2012-09-14 11:05:03 +0200

    priem efficienter gemaakt

--- week2/Priem/priem.c
+++ week2/Priem/priem.c
@@ -5,20 +5,22 @@ int main() {
   int invoer;
   printf("Van welk getal moet bepaald worden of het priem is: ") ;
   scanf("%d", &invoer);
-  int index;					/*Index is voor het afgaan van de loop.*/
+  int index;					/* Index is voor het afgaan van de loop. */
   
-  if(invoer==1||invoer==0){
-      printf("%d is geen priem \n", invoer);
-      return 0;
-    }
+  if (invoer == 0 || invoer == 1 || invoer % 2 == 0) { /* Checkt of het om een randgeval gaat, of of het een even getal is. */
+    printf("%d is geen priem \n", invoer);
+    return 0;
+  }
   
-  for(index=2; index<invoer; index++){
-    if(invoer%index==0){
+  for(index = 3; index <= invoer / 3; index += 2){ /* Alle even getallen kun je wel overslaan, want dan zou het ook al deelbaar door 2 zijn geweest. */
+    if(invoer % index == 0 || invoer == 1 || invoer == 0){
       printf("%d is geen priem \n", invoer);
-      return 0;					/* stap uit het programma als zeker is dat het geen priem is*/
+      return 0;					/* Stap uit het programma als zeker is dat het geen priem is*/
     }
   }
+  
+  
+  /* Als het programma hier komt zijn alle tests doorstaan, dus is het een priemgetal */
   printf("%d is priem \n", invoer);
-    
   return 0 ;
 }
