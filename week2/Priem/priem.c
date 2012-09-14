5f1ad20c7d796cf88fab90afd6848e9afd82645f
HanKruiger <hankruiger@gmail.com>
2012-09-14 11:43:29 +0200

    nu werkt priem weer.

--- week2/Priem/priem.c
+++ week2/Priem/priem.c
@@ -7,20 +7,24 @@ int main() {
   scanf("%d", &invoer);
   int index;					/* Index is voor het afgaan van de loop. */
   
+  if (invoer == 2) {
+    printf("%d is priem\n", invoer);
+    return 0;
+  }
+  
   if (invoer == 0 || invoer == 1 || invoer % 2 == 0) { /* Checkt of het om een randgeval gaat, of of het een even getal is. */
-    printf("%d is geen priem \n", invoer);
+    printf("%d is geen priem\n", invoer);
     return 0;
   }
   
   for(index = 3; index <= invoer / 3; index += 2){ /* Alle even getallen kun je wel overslaan, want dan zou het ook al deelbaar door 2 zijn geweest. */
-    if(invoer % index == 0 || invoer == 1 || invoer == 0){
-      printf("%d is geen priem \n", invoer);
+    if(invoer % index == 0){
+      printf("%d is geen priem\n", invoer);
       return 0;					/* Stap uit het programma als zeker is dat het geen priem is*/
     }
   }
   
-  
   /* Als het programma hier komt zijn alle tests doorstaan, dus is het een priemgetal */
-  printf("%d is priem \n", invoer);
+  printf("%d is priem\n", invoer);
   return 0 ;
 }
