edaa050bed9a76d5380cf88d7eca7ad2dc42db65
HanKruiger <hankruiger@gmail.com>
2012-09-14 12:40:21 +0200

    Comments erbij, en overbodige conditie weg.

--- week2/markt/markt.c
+++ week2/markt/markt.c
@@ -12,42 +12,43 @@ int main() {
   scanf("%d", &meloenPrijs);
   putchar('\n');
   
-  int maxsinaas, maxgrape, maxmel;
+  int maxSinaas, maxGrape, maxMel;
   if(sinaasappelPrijs==0){
-    maxsinaas= 99;
+    maxSinaas= 99; /* 99, want er moet minimaal 1 andere vrucht bij. Anders kom je niet aan 100 euro. */
   } else {
-    maxsinaas = 10000 / sinaasappelPrijs;
+    maxSinaas = 10000 / sinaasappelPrijs; /* Hoeveel sinaasappels kan je kopen voor 100 euro? */
   }
-
   
   printf("Sinaasappel\tGrapefruit\tMeloen\n");
   
-  int sinaasindex;
-  int grapeindex;
+  int sinaasIndex;
+  int grapeIndex;
   
-  int geldsinaas;
-  int geldgrape;
-  int geldmel;
+  /* Wordt gebruikt voor het totale bedrag dat besteed wordt aan de bijbehorende vrucht. */
+  int geldSinaas;
+  int geldGrape;
+  int geldMel;
   
-  for(sinaasindex=0; sinaasindex<=maxsinaas; sinaasindex++){
+  for(sinaasIndex=0; sinaasIndex<=maxSinaas; sinaasIndex++){
 
     if(grapefruitPrijs==0){
-      maxgrape = 100 - sinaasindex;
+      maxGrape = 100 - sinaasIndex; /* Als grapefruits gratis zijn is het maximaal aantal grapefruits 100 min het aantal sinaasappels dat je al hebt.  */
     } else {
-      maxgrape = (10000 - sinaasindex * sinaasappelPrijs)/grapefruitPrijs;
+      maxGrape = (10000 - sinaasIndex * sinaasappelPrijs)/grapefruitPrijs; /* Anders is het maximaal aantal grapefruits afhankelijk van het geld dat je nog nodig hebt. */
     }
     
-    geldsinaas= sinaasindex * sinaasappelPrijs;
-    for(grapeindex = 0; grapeindex <= maxgrape; grapeindex++){
-      maxmel = 100 - sinaasindex - grapeindex;
-      if (maxmel < 0) {
+    geldSinaas= sinaasIndex * sinaasappelPrijs;
+    for(grapeIndex = 0; grapeIndex <= maxGrape; grapeIndex++){
+      maxMel = 100 - sinaasIndex - grapeIndex; /* Bepaalt het resterende aantal meloenen om 100 vruchten te krijgen. */
+      if (maxMel < 0) { /* Het is mogelijk dat maxMel negatief wordt. Als dit gebeurt, ga uit de nested for-loop, want grapeIndex is toch al te hoog, en wordt anders alleen maar hoger. */
 	break;
       }
-      geldgrape = grapeindex * grapefruitPrijs;
-      geldmel = maxmel * meloenPrijs;
       
-      if(10000 - (geldsinaas + geldgrape + geldmel) == 0 && (sinaasindex + grapeindex + maxmel == 100)){
-	printf("%d\t\t%d\t\t%d \n", sinaasindex, grapeindex, maxmel);
+      geldGrape = grapeIndex * grapefruitPrijs;
+      geldMel = maxMel * meloenPrijs;
+      
+      if(10000 - (geldSinaas + geldGrape + geldMel) == 0){ /* Check uiteindelijk of het totale bedrag 100 euro is. Als dit zo is: Print combinatie. */
+	printf("%d\t\t%d\t\t%d \n", sinaasIndex, grapeIndex, maxMel);
       }
     }
   }
